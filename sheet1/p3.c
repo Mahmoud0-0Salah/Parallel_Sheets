790ac9ecd71ec10422449259b606d55b629147b3
Mahmoud0-0Salah <mahmoudsalah2339@gmail.com>
2025-05-14 09:29:33 +0300

    Solve but in p3.c

--- sheet1/p3.c
+++ sheet1/p3.c
@@ -5,30 +5,28 @@ typedef struct {int* arr;int s;int e;int res;} SumData;
 
 void * solve(void* arg)
 {
-	SumData* s = (SumData*)arg;
-	s->res=0;
-	for (int i=s->s;i<=s->e;i++)
-		s->res+=s->arr[i];
-	return s->res;
+        SumData* s= (SumData*) arg;
+        for (int i = s->s;i<=s->e;i++)
+                s->res+=s->arr[i];
+        return s;
 }
 
 int main(int argc,char* argv[])
 {
-	int ans =0;
-	int arr[] ={1,2,3,4,5,6};
-	pthread_t th[2];
-	SumData data[2];
-	data[0].arr=arr;data[0].s=0;data[0].e=2;
-	data[1].arr=arr;data[1].s=3;data[1].e=5;
+        int ans =0;
+        int arr[] ={1,2,3,4,5,6};
+        pthread_t th[2];
+        SumData data[2];
+        data[0].arr=arr;data[0].s=0;data[0].e=2;
+        data[1].arr=arr;data[1].s=3;data[1].e=5;
 
-	pthread_create(&th[0],NULL,solve,&data[0]);
-	pthread_create(&th[1],NULL,solve,&data[1]);
+        pthread_create(&th[0],NULL,solve,&data[0]);
+        pthread_create(&th[1],NULL,solve,&data[1]);
 
-	SumData* r;
-	pthread_join(th[0],(void**) &r);
-	ans+=r->res;
-	pthread_join(th[1],(void**) &r);
-	ans+=r->res;
-
-	printf("result = %d\n",ans);
+        SumData* r;
+        pthread_join(th[0],(void )&r);
+        ans+=r->res;
+        pthread_join(th[0],(void )&r);
+        ans+=r->res;
+        printf("result = %d\n",ans);
 }
